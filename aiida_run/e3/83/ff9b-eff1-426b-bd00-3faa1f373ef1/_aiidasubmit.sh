95f8829878e295904be54d9e700630b5cead9890
Hiroto-Saito1 <hiroto.saito.t5@dc.tohoku.ac.jp>
2025-01-29 07:20:11 +0900

    qe test done

--- aiida_run/e3/83/ff9b-eff1-426b-bd00-3faa1f373ef1/_aiidasubmit.sh
+++ aiida_run/e3/83/ff9b-eff1-426b-bd00-3faa1f373ef1/_aiidasubmit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -r n
 #PBS -m n
-#PBS -N aiida-143
+#PBS -N aiida-167
 #PBS -V
 #PBS -o _scheduler-stdout.txt
 #PBS -e _scheduler-stderr.txt
@@ -9,5 +9,7 @@
 #PBS -l select=1:mpiprocs=1
 cd "$PBS_O_WORKDIR"
 
+
+export PATH=/opt/pbs/bin:$PATH
 
 'mpirun' '-np' '1' '/home/hirotosaito/codes/qe-7.3_mag/PW/src/pw.x' '-in' 'aiida.in'  > 'aiida.out'
